996706bcd9dec9a4b36bb14d04022d18164bc504
Andrey Brekotkin <brekot@ya.ru>
2021-10-19 00:31:12 +0300

    animation to change

--- src/js/index.js
+++ src/js/index.js
@@ -1,24 +1,102 @@
 import $ from "jquery";
+import AOS from 'aos';
 
 window.jQuery = $;
 window.$ = $;
 
+AOS.init();
+
+import Motus from './lib/motus.web.js';
+
+[].forEach.call(document.getElementsByClassName('paralax-left'), function (el) {
+
+    var animation = new Motus.Animation({
+        $el: el,
+        keyframes: {
+            40: {
+                left: {
+                    from: 0,
+                    to: 50,
+                    unit: "%"
+                },
+                opacity: {
+                    from: 0,
+                    to: 1,
+                },
+            },
+            80: {
+                left: {
+                    to: 50,
+                    unit: "%"
+                },
+            },
+            100: {
+                left: {
+                    from: 50,
+                    to: 100,
+                    unit: "%"
+                },
+                opacity: {
+                    from: 1,
+                    to: 0,
+                },
+            },
+        }
+    });
+    
+    Motus.addAnimation(animation);
+});
+
+[].forEach.call(document.getElementsByClassName('paralax-right'), function (el) {
+
+    var animation = new Motus.Animation({
+        $el: el,
+        keyframes: {
+            40: {
+                right: {
+                    from: 0,
+                    to: 50,
+                    unit: "%"
+                },
+                opacity: {
+                    from: 0,
+                    to: 1,
+                },
+            },
+            80: {
+                right: {
+                    to: 50,
+                    unit: "%"
+                },
+            },
+            100: {
+                right: {
+                    from: 50,
+                    to: 100,
+                    unit: "%"
+                },
+                opacity: {
+                    from: 1,
+                    to: 0,
+                },
+            },
+        }
+    });
+    
+    Motus.addAnimation(animation);
+});
+
 $(function() {
 
-    // Cache selectors
     var lastId,
         topMenu = $(".main-nav"),
         topMenuHeight = topMenu.outerHeight()+15,
-        // All list items
         menuItems = topMenu.find(".main-nav__link"),
-        // Anchors corresponding to menu items
         scrollItems = menuItems.map(function(){
             var item = $($(this).attr("href"));
             if (item.length) { return item; }
         });
 
-    // Bind click handler to menu items
-    // so we can get a fancy scroll animation
     menuItems.on('click', function(e){
 
         var href = $(this).attr("href"),
@@ -33,17 +111,15 @@ $(function() {
         e.preventDefault();
     });
 
-    // Bind to scroll
     $(window).on('scroll', function(){
-    // Get container scroll position
-    var fromTop = $(this).scrollTop()+topMenuHeight;
-    
-    // Get id of current scroll item
+
+        var fromTop = $(this).scrollTop()+topMenuHeight;
+
         var cur = scrollItems.map(function(){
 
             if ($(this).offset().top < fromTop) return this;
         });
-        // Get the id of the current element
+
         cur = cur[cur.length-1];
 
         var id = cur && cur.length ? cur[0].id : "";
@@ -120,3 +196,59 @@ $(function() {
 		return false;
 	});
 });
+
+var isScrolling = false;
+
+window.addEventListener("scroll", throttleScroll, false);
+
+function throttleScroll(e)
+{
+    if (isScrolling == false)
+    {
+        window.requestAnimationFrame(function() {
+
+            dealWithScrolling(e);
+
+            isScrolling = false;
+        });
+    }
+
+    isScrolling = true;
+}
+
+function dealWithScrolling(e)
+{
+    [].forEach.call(document.getElementsByClassName('animate-elem'), function (el) {
+        
+        if (isFullyVisible(el))
+        {
+            el.classList.add('animate-active');
+        }
+
+        if (!isPartiallyVisible(el))
+        {
+            el.classList.remove('animate-active');
+        }
+    });
+}
+
+function isPartiallyVisible(el)
+{
+    var elementBoundary = el.getBoundingClientRect();
+
+    var top = elementBoundary.top;
+    var bottom = elementBoundary.bottom;
+    var height = elementBoundary.height;
+
+    return ((top + height >= 0) && (height + window.innerHeight >= bottom));
+}
+
+function isFullyVisible(el)
+{
+    var elementBoundary = el.getBoundingClientRect();
+   
+    var top = elementBoundary.top;
+    var bottom = elementBoundary.bottom;
+   
+    return ((top >= 0) && (bottom <= window.innerHeight));
+}
